9053ecd44ce5d504d289997ba7d942989f827045
Aaron <aaron@bettercoder.net>
2016-01-31 14:32:09 -0800

    Fix API breakages.

--- gitio.go
+++ gitio.go
@@ -5,18 +5,25 @@ package gitio
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
 // Shorten a long github url.
 func Shorten(longurl string) (string, error) {
-	resp, err := http.PostForm("http://git.io", url.Values{`url`: {longurl}})
+	resp, err := http.PostForm(`https://git.io/create`, url.Values{`url`: {longurl}})
 	if err != nil {
 		return "", nil
 	}
-	if resp.StatusCode != 201 {
-		return "", fmt.Errorf("Expected 201 response, got: %d", resp.StatusCode)
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("Expected 200 response, got: %d", resp.StatusCode)
 	}
-	return resp.Header.Get("Location"), nil
+
+	text, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Error reading response from gitio: %v", err)
+	}
+
+	return fmt.Sprintf(`https://git.io/%s`, text), nil
 }
